a3bf35203a1435239f689efd8ff8689557920c21
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-04 21:52:31 +0200

    ajout de du fichier css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./index.css";
 
-// Comment
-// Stateless functional component
-// Return always a JSX
-function Greeting() {
-  return <h2>Optimus-ged cr3sc3nd0 is the Best</h2>;
-}
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img src="../assets/images/love-you.PNG" alt="father's love book" />
+);
+const Title = () => <h2>This is a book about father's love</h2>;
+const Author = () => <p>Optimus ged</p>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
