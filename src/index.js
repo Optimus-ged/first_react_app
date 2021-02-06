24608e884a7d241eaad65282c7cdb27e86b0f3db
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-06 21:52:18 +0200

    repetition on index js module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+// Comment
+// Importing important packages for developement
 import React from "react";
 import ReactDom from "react-dom";
+
+// Comment
+// Importing css file
 import "./index.css";
+
+// Comment
+// Staless functional component
+// Always return JSX
 const BookList = () => {
   return (
     <section className="book-list">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
       <Book />
     </section>
   );
@@ -12,8 +25,8 @@ const BookList = () => {
 const Book = () => {
   return (
     <article className="book">
-      <img src="" alt="place a la toph" />
-      <h2>Father's love book</h2>
+      <img src="../assets/images/love-you.PNG" alt="father's love book" />
+      <h2>This is a book about father's love</h2>
       <p>Optimus ged</p>
     </article>
   );
