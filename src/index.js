ddfb440fbe1f8a25600765ab6fac93fefd036a26
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-08 22:47:20 +0200

    repetition on css exercices

--- src/index.js
+++ src/index.js
@@ -4,28 +4,29 @@ import "./index.css";
 
 const Team = () => {
   return (
-    <section className="wrapper">
+    <article className="wrapper">
       <h1>Our Team</h1>
-      <article className="team">
-        <TeamMember name="Optimus Ged" role="UI Developper" />
-        <TeamMember name="Cr3sc3ndo Op" role="Mob Developper" />
-        <TeamMember name="Gedeon Yala" role="UI Developper" />
-      </article>
-    </section>
+      <section className="team">
+        <TeamMember name="Optimus ged" role="Mob Developper" />
+        <TeamMember name="Yala Gedeon" role="Front-end Dev" />
+        <TeamMember name="Gedeon Yala" role="Back-end Dev" />
+      </section>
+    </article>
   );
 };
 
 const TeamMember = (props) => {
   return (
     <div className="team-member">
-      <div className="team-image">{/* <img src="" alt=""/> */}</div>
+      <div className="team-image">
+        
+      </div>
       <h3>{props.name}</h3>
       <p className="role">{props.role}</p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe
-        inventore at repudiandae suscipit corporis necessitatibus. Aut veritatis
-        omnis libero, perspiciatis illum id voluptatum. Esse, animi itaque
-        asperiores nisi fugit voluptatum?
+      <p className="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum veniam
+        porro blanditiis? Ipsa odio quisquam quos, labore, quae ipsum tenetur,
+        eaque eum totam recusandae veritatis?
       </p>
     </div>
   );
