d1ed09aa91c2126ad6dfae9039a13fc0170fe975
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-08 22:12:37 +0200

    exercises in css

--- src/index.js
+++ src/index.js
@@ -2,45 +2,33 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const BookList = () => {
+const Team = () => {
   return (
-    <section className="book-list">
-      <Book
-        title="This book is talking about father's love"
-        author="Optimus ged"
-      />
-      <Book
-        title="This book is talking about father's love"
-        author="Cr3sc3nd0 Optimus"
-      />
-      <Book
-        title="This book is talking about father's love"
-        author="John Yala"
-      />
-      <Book
-        title="This book is talking about father's love"
-        author="Sisi Sifa"
-      />
-      <Book
-        title="This book is talking about father's love"
-        author="Nzolani Salem"
-      />
-      <Book
-        title="This book is talking about father's love"
-        author="Adele Bash"
-      />
+    <section className="wrapper">
+      <h1>Our Team</h1>
+      <article className="team">
+        <TeamMember name="Optimus Ged" role="UI Developper" />
+        <TeamMember name="Cr3sc3ndo Op" role="Mob Developper" />
+        <TeamMember name="Gedeon Yala" role="UI Developper" />
+      </article>
     </section>
   );
 };
 
-const Book = (props) => {
+const TeamMember = (props) => {
   return (
-    <article className="book">
-      <img src="" alt="" />
-      <h2>{props.title}</h2>
-      <p>{props.author}</p>
-    </article>
+    <div className="team-member">
+      <div className="team-image">{/* <img src="" alt=""/> */}</div>
+      <h3>{props.name}</h3>
+      <p className="role">{props.role}</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe
+        inventore at repudiandae suscipit corporis necessitatibus. Aut veritatis
+        omnis libero, perspiciatis illum id voluptatum. Esse, animi itaque
+        asperiores nisi fugit voluptatum?
+      </p>
+    </div>
   );
 };
 
-ReactDom.render(<BookList />, document.getElementById("root"));
+ReactDom.render(<Team />, document.getElementById("root"));
