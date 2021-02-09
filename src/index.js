78a6d40126ebc48fcd3db5ba8837e11472d152c5
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-09 22:42:03 +0200

    final repetition on this exercise

--- src/index.js
+++ src/index.js
@@ -4,33 +4,30 @@ import "./index.css";
 
 const Team = () => {
   return (
-    <article className="wrapper">
-      <h1>Our team</h1>
-      <section className="team">
-        <TeamMember name="Optimus ged" role="Mob developper" />
-        <TeamMember name="Paul Doe" role="Tester" />
-        <TeamMember name="Rosy Meg ged" role="Support Lead" />
-      </section>
-    </article>
+    <section className="wrapper">
+      <h1>Our Team</h1>
+      <article className="team">
+        <TeamMember name="Paul Doe" role="UI Developper" />
+        <TeamMember name="Rosy Meg" role="Tester" />
+        <TeamMember name="Alex Wood" role="Support Lead" />
+      </article>
+    </section>
   );
 };
 
 const TeamMember = (props) => {
   return (
-    <section className="team-member">
+    <div className="team-member">
       <div className="team-image"></div>
       <h3>{props.name}</h3>
       <p className="role">{props.role}</p>
-      <p>
-        <div className="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut est nulla
-          quibusdam, architecto natus non officia eligendi voluptas? Deleniti
-          in, aliquam iste blanditiis distinctio quas totam aspernatur ipsum
-          natus obcaecati?
-        </div>
+      <p className="content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        blanditiis ad totam praesentium neque ut quibusdam nulla inventore error
+        natus, maiores eum explicabo quos ipsum.
       </p>
-    </section>
+    </div>
   );
 };
 
-ReactDom.render(<Team />, document.getElementById("root"));
+ReactDom.render(<Team/>, document.getElementById('root'));
