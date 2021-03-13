4a03bc507bd8d397cf4c36f78ddbd7518aaa183c
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-13 21:51:07 +0200

    Repetition on index html

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import React from "react";
-import ReactDom from "react-dom";
-import "./index.css";
+import ReactDom from 'react-dom';
 
-const Team = () => {
+const Team = ()=>{
   return (
-    <section className="wrapper">
+    <article>
       <h1>Our Team</h1>
-      <article className="team">
-        <TeamMember name="Paul Doe" role="UI Developper" />
-        <TeamMember name="Rosy Meg" role="Tester" />
-        <TeamMember name="Alex Wood" role="Support Lead" />
-      </article>
-    </section>
+    </article>
   );
 };
 
-const TeamMember = (props) => {
-  return (
-    <div className="team-member">
-      <div className="team-image"></div>
-      <h3>{props.name}</h3>
-      <p className="role">{props.role}</p>
-      <p className="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-        blanditiis ad totam praesentium neque ut quibusdam nulla inventore error
-        natus, maiores eum explicabo quos ipsum.
-      </p>
-    </div>
-  );
-};
-
-ReactDom.render(<Team/>, document.getElementById('root'));
+ReactDom.render(<Team/>, document.getElementById("root"));
