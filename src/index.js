9452ff5882f580bed607dafc86e06c0edc320d64
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-14 21:56:10 +0200

    Repetition on css file for more comprehension

--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
-// Importing dependancies
+// importing dependancies
 import React from "react";
 import ReactDom from "react-dom";
+import "./index.css";
 
-// Building Team component
+// Building Team Component
 const Team = () => {
   return (
     <article className="wrapper">
-      <h1>Cr3sc3nd0'Z Team</h1>
+      <h1>Cr3sc3nd0'Z</h1>
       <section className="team">
-        <TeamMember name="Salem Nzolani" poste="Comptable" />
-        <TeamMember name="Jeannette Sham" poste="Caissiere" />
-        <TeamMember name="Queen Kafene" poste="Secretaire" />
+        <TeamMember name="Gedeon Yala" role="President Directeur" />
+        <TeamMember name="Gedeon Yala" role="President Directeur" />
+        <TeamMember name="Gedeon Yala" role="President Directeur" />
       </section>
     </article>
   );
 };
 
-// Building Team Member
+// Building Team-member component
 const TeamMember = (props) => {
   return (
     <div className="team-member">
-      <div className="member-picture"></div>
+      <div className="member-image"></div>
       <h3>{props.name}</h3>
-      <p className="poste">{props.poste}</p>
-      <p className="content">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
-        explicabo nam, tempora numquam ipsa alias iste! Delectus amet
-        consequatur dolore provident aliquid placeat aspernatur, illo unde,
-        commodi blanditiis minima veritatis.
+      <p className="role">{props.role}</p>
+      <p className="content" >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos facere
+        impedit, earum dolore sint dolores corporis architecto consequuntur quos
+        officia voluptatum explicabo in, ex placeat distinctio laboriosam dicta
+        obcaecati dolor.
       </p>
     </div>
   );
