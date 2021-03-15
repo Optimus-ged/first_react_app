db8171172d662c64d5d406b9efdc962301405c17
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-15 23:43:29 +0200

    Repetition on use of gradient colors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-// Importing depedancies
+// Importing dependancies and modules
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-// Building HomePage component
+// Building HomePage Component
 const HomePage = () => {
   return (
     <article className="home-page">
-      <div className="menu">
-        <ul>
-          <li>Home</li>
-          <li>Users</li>
-          <li>Clients</li>
-          <li>Setings</li>
-          <li>Exit</li>
-        </ul>
-      </div>
+      <ul className="menu">
+        <li>Home</li>
+        <li>Users</li>
+        <li>Clients</li>
+        <li>Settings</li>
+        <li>Exit</li>
+      </ul>
+      <p className="signup-btn">
+        <span>Sign-Up</span>
+      </p>
     </article>
   );
 };
 
-// Exporting module
+// Exporting Module
 ReactDom.render(<HomePage />, document.getElementById("root"));
