d73de05dd33374fb47317711bc26f15da761be6c
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-15 22:35:38 +0200

    Handling of ul list

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
-// importing dependancies
+// Importing depedancies
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-// Building Team Component
-const Team = () => {
+// Building HomePage component
+const HomePage = () => {
   return (
-    <article className="wrapper">
-      <h1>Cr3sc3nd0'Z</h1>
-      <section className="team">
-        <TeamMember name="Gedeon Yala" role="President Directeur" />
-        <TeamMember name="Gedeon Yala" role="President Directeur" />
-        <TeamMember name="Gedeon Yala" role="President Directeur" />
-      </section>
+    <article className="home-page">
+      <div className="menu">
+        <ul>
+          <li>Home</li>
+          <li>Users</li>
+          <li>Clients</li>
+          <li>Setings</li>
+          <li>Exit</li>
+        </ul>
+      </div>
     </article>
   );
 };
 
-// Building Team-member component
-const TeamMember = (props) => {
-  return (
-    <div className="team-member">
-      <div className="member-image"></div>
-      <h3>{props.name}</h3>
-      <p className="role">{props.role}</p>
-      <p className="content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos facere
-        impedit, earum dolore sint dolores corporis architecto consequuntur quos
-        officia voluptatum explicabo in, ex placeat distinctio laboriosam dicta
-        obcaecati dolor.
-      </p>
-    </div>
-  );
-};
-
 // Exporting module
-ReactDom.render(<Team />, document.getElementById("root"));
+ReactDom.render(<HomePage />, document.getElementById("root"));
