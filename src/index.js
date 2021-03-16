eaf8524c7a258e06fdc27d637445dc2039d9dfe5
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-16 14:36:12 +0200

    An other repetition maded

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-//Importing dependancies and modules
+// Importing depeendancies and modules
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
 // Building HomePage component
 const HomePage = () => {
-  return (
-    <article className="home-page">
-      <ul className="menu">
-        <li>Home</li>
-        <li>Clients</li>
-        <li>Users</li>
-        <li>About</li>
-        <li>Exit</li>
-      </ul>
-      <p className="signup-btn">Sign-Up</p>
-    </article>
-  );
+  return <article className="home-page">
+    <ul>
+      <li>Home</li>
+      <li>Clients</li>
+      <li>Users</li>
+      <li>Settings</li>
+      <li>Exit</li>
+    </ul>
+    <p className="signup-btn">
+      Sign-Up
+    </p>
+  </article>;
 };
 
 // Exporting module
