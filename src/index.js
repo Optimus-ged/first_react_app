366ace3fc993e838e6e345fa6c6e2c6b9549c912
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-16 21:23:04 +0200

    Repetition made successfully on modules

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
+
+// Constants
 import Bit from "./images/bitcoin.png";
 
 // Building Home-page component
@@ -11,24 +13,25 @@ const HomePage = () => {
       <div className="nav-bar">
         <ul>
           <li>Home</li>
-          <li>Users</li>
           <li>Clients</li>
-          <li>Settings</li>
+          <li>Users</li>
+          <li>About</li>
           <li>Exit</li>
         </ul>
+        <p className="signup-btn">Sign-Up</p>
       </div>
       <div className="banner">
         <section className="app-text">
           <h1>Our besic thesis for bitcoin is that is the better than Gold</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-            ducimus nam odit quia vero numquam? Ratione, nostrum et at natus,
-            quae minus placeat consectetur facere magni officiis voluptatibus ut
-            tempore.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel
+            maiores hic quia nihil, ex eveniet a alias voluptate eius enim
+            dignissimos mollitia minus inventore, nostrum reprehenderit natus,
+            fuga dolores quis.
           </p>
         </section>
-        <section className="app-image">
-          <img src={Bit} alt="Bitcoin image"/>
+        <section className="app-toph">
+          <img src={Bit} alt="yyy" />
         </section>
       </div>
     </article>
