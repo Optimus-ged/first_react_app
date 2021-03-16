aff6f4c0622b2e32cb8e3edae6524fabfb1efbb0
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-16 18:19:10 +0200

    An other repetition made correctely

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
-// Importing depeendancies and modules
+// Importing depedancies and modules
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-import bit from "./images/bitcoin.png";
+import Bit from "./images/bitcoin.png";
 
-// Building HomePage component
+// Building Home-page component
 const HomePage = () => {
   return (
     <article className="home-page">
       <div className="nav-bar">
         <ul>
           <li>Home</li>
-          <li>Clients</li>
           <li>Users</li>
+          <li>Clients</li>
           <li>Settings</li>
           <li>Exit</li>
         </ul>
       </div>
-      {/* <p className="signup-btn">
-        <span> Sign-Up</span>
-      </p> */}
       <div className="banner">
-        <div className="app-text">
+        <section className="app-text">
           <h1>Our besic thesis for bitcoin is that is the better than Gold</h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />{" "}
-            Odit, labore magni? Est doloribus vel deleniti quaerat <br />{" "}
-            explicabo incidunt necessitatibus omnis autem voluptatem, <br />{" "}
-            atque sit in rerum. Assumenda necessitatibus <br /> reprehenderit
-            corrupti ?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+            ducimus nam odit quia vero numquam? Ratione, nostrum et at natus,
+            quae minus placeat consectetur facere magni officiis voluptatibus ut
+            tempore.
           </p>
-        </div>
-        <div className="app-picture">
-          <img src={bit} alt="ggg" />
-        </div>
+        </section>
+        <section className="app-image">
+          <img src={Bit} alt="Bitcoin image"/>
+        </section>
       </div>
     </article>
   );
