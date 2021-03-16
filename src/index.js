eeec8d7ed71c5e9cb657d46c83b71de34479ce79
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-16 17:01:15 +0200

    Try to use asset images in React

--- src/index.js
+++ src/index.js
@@ -5,18 +5,35 @@ import "./index.css";
 
 // Building HomePage component
 const HomePage = () => {
-  return <article className="home-page">
-    <ul>
-      <li>Home</li>
-      <li>Clients</li>
-      <li>Users</li>
-      <li>Settings</li>
-      <li>Exit</li>
-    </ul>
-    <p className="signup-btn">
-      Sign-Up
-    </p>
-  </article>;
+  return (
+    <article className="home-page">
+      <ul>
+        <li>Home</li>
+        <li>Clients</li>
+        <li>Users</li>
+        <li>Settings</li>
+        <li>Exit</li>
+      </ul>
+      {/* <p className="signup-btn">
+        <span> Sign-Up</span>
+      </p> */}
+      <div className="banner">
+        <div className="app-text">
+          <h1>Our besic thesis for bitcoin is that is the better than Gold</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />{" "}
+            Odit, labore magni? Est doloribus vel deleniti quaerat <br />{" "}
+            explicabo incidunt necessitatibus omnis autem voluptatem, <br />{" "}
+            atque sit in rerum. Assumenda necessitatibus <br /> reprehenderit
+            corrupti ?
+          </p>
+        </div>
+        <div className="app-picture">
+          <img src={require("./images/bitcoin.png")} alt="error" />
+        </div>
+      </div>
+    </article>
+  );
 };
 
 // Exporting module
