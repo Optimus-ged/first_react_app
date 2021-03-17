9efc4edf37e24a01dd8069a8f4f352beb9648a94
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-17 16:45:18 +0200

    An other repeat on use of pictures and position in css

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-// Importing depedancies and modules
+// Importing module and dependancies
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 import Bit from "./images/bitcoin.png";
 
-// Building Home-page component
+// Building HomePage component
 const HomePage = () => {
   return (
     <article className="home-page">
-      <div className="nav-bar">
+      <section className="nav-bar">
         <ul>
-          <li>Home</li>
-          <li>Users</li>
+          <li>Acceuil</li>
           <li>Clients</li>
-          <li>Settings</li>
+          <li>Users</li>
+          <li>Aide</li>
           <li>Exit</li>
         </ul>
-        <p className="signup-btn">Sign-Up</p>
-      </div>
-      <div className="banner">
-        <section className="app-text">
+        <p className="enterprise">Optimus Tec</p>
+      </section>
+      <section className="banner">
+        <div className="app-text">
           <h1>Our besic thesis for bitcoin is that is the better than Gold</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-            ducimus nam odit quia vero numquam? Ratione, nostrum et at natus,
-            quae minus placeat consectetur facere magni officiis voluptatibus ut
-            tempore.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            autem quas totam vel voluptates explicabo, aut recusandae velit
+            praesentium eligendi distinctio id eum suscipit quasi non illo
+            corrupti iste temporibus!
           </p>
-        </section>
-        <section className="app-toph">
+        </div>
+        <div className="app-toph">
           <img src={Bit} alt="Bitcoin toph" />
-        </section>
-      </div>
+        </div>
+      </section>
     </article>
   );
 };
