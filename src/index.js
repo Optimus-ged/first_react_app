e209e72c10b9f785654a461f50b691792afbe8b2
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-18 20:58:12 +0200

    An other repeat maded on use of colors and positionning in css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Importing modules and dependancies
-import Reat from "react";
+import React from "react";
 import ReactDom from "react-dom";
-import './index.css';
-import bitcoin from './images/bitcoin.png';
+import "./index.css";
+import bit from './images/bitcoin.png';
 
 // Building components
 const Wrapper = () => {
@@ -14,16 +14,16 @@ const Wrapper = () => {
             <a href="www.google.com">Home</a>
           </li>
           <li>
-            <a href="www.google.com">Users</a>
+            <a href="www.facebook.com">Users</a>
           </li>
           <li>
-            <a href="www.google.com">Clients</a>
+            <a href="www.twitter.com">Clients</a>
           </li>
           <li>
-            <a href="www.google.com">Settings</a>
+            <a href="www.instagram.com">About</a>
           </li>
           <li>
-            <a href="www.google.com">About</a>
+            <a href="www.whatsap.com">Exit</a>
           </li>
         </ul>
         <p className="optimus">OptimusTec</p>
@@ -32,14 +32,14 @@ const Wrapper = () => {
         <div className="app-text">
           <h1>Optimus code for bitcoin witch is the better than Gold</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-            sequi dignissimos delectus similique animi quam illum impedit,
-            nesciunt officia. Similique at est alias dicta dolorum, sit harum
-            sequi praesentium quisquam?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+            nobis modi consequuntur, rem nisi, iure laborum quos molestiae
+            possimus ex veniam atque est animi, totam dignissimos minus
+            doloremque. Tempore, dolor?
           </p>
         </div>
         <div className="app-toph">
-          <img src={bitcoin} alt="bitcoin toph"/>
+          <img src={bit} alt="toph bitcoin"/>
         </div>
       </section>
     </article>
