b083984a6b58e985a2d7fe5e39c4e79879155b32
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-20 20:47:58 +0200

    An other repeat maded on use of hover event

--- src/index.js
+++ src/index.js
@@ -1,50 +1,51 @@
-// importing modules and dependancies
+// Importing depedancies and moduels
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 import bitcoin from "./images/bitcoin.png";
 
-// Building components
+// Building wrapper componen
 const Wrapper = () => {
   return (
     <article className="wrapper">
       <section className="nav-bar">
         <ul>
           <li>
-            <a href="http//www.google.com">Home</a>
+            <a href="www.google.com">Home</a>
           </li>
           <li>
-            <a href="http//www.google.com">Client</a>
+            <a href="www.google.com">Users</a>
           </li>
           <li>
-            <a href="http//www.google.com">Users</a>
+            <a href="www.google.com">Clients</a>
           </li>
           <li>
-            <a href="http//www.google.com">Settings</a>
+            <a href="www.google.com">About</a>
           </li>
           <li>
-            <a href="http//www.google.com">Exit</a>
+            <a href="www.google.com">Exit</a>
           </li>
         </ul>
-        <p className="optimus">OptimusTec</p>
+        <p className="optimus">Optimus Tec</p>
       </section>
       <section className="banner">
         <div className="app-text">
           <h1>Optimus code for bitcoin witch is the better than Gold</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
-            illo voluptatibus eveniet recusandae quod porro. At necessitatibus
-            dicta libero eum! Eos, ducimus repellendus mollitia rem quos ad
-            officia error cum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Distinctio, quos, dicta explicabo nesciunt itaque autem error
+            consequatur labore reprehenderit nobis possimus. Assumenda, itaque.
+            Assumenda explicabo delectus accusantium, nesciunt aliquid
+            distinctio.
           </p>
         </div>
         <div className="app-toph">
-          <img src={bitcoin} alt="bitcoin toph" />
+          <img src={bitcoin} alt="Erreur de chargement"/>
         </div>
       </section>
     </article>
   );
 };
 
-// Exporting module
+// Exporting our component
 ReactDom.render(<Wrapper />, document.getElementById("root"));
