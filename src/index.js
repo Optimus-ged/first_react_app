ee0b0c300f5ee1fc6224349cbe666b634fad0585
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-23 22:43:20 +0200

    An other repeat on header builing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Importing modules and dependancies
+// Importing dependancies and modules
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
@@ -8,12 +8,12 @@ const Wrapper = () => {
   return (
     <article className="wrapper">
       <section className="nav-bar">
-        <div className="logo">CODDING MARKET</div>
-        <div className="top-menu">
+        <div className="logo">Coding Market</div>
+        <div className="top-header">
           <div className="home-link">Home</div>
-          <div className="right-info">
-            <div className="user-btn">Users</div>
-            <div className="client-btn">Clients</div>
+          <div className="right-menus">
+            <p className="user-btn">User</p>
+            <p className="client-btn">Client</p>
           </div>
         </div>
       </section>
