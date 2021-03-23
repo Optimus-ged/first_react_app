bce1a4100e9971754964f9d819a1dd07d5a33b47
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-23 21:04:37 +0200

    An other disposition maded

--- src/index.js
+++ src/index.js
@@ -3,29 +3,22 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-// Building components
+// Building Wrapper component
 const Wrapper = () => {
   return (
     <article className="wrapper">
-      <div className="dash-board">
+      <div className="dash-board"></div>
+      <div className="banner">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi hic
-          velit deserunt incidunt obcaecati atque nostrum fuga suscipit libero
-          facilis! Eligendi odit cumque architecto eos optio consequatur maxime
-          unde voluptatum?
-        </p>
-      </div>
-      <div className="content">
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis,
-          voluptas natus! Fugiat, tempora ullam quisquam numquam, ipsam porro
-          corrupti quod dolor reprehenderit quasi nihil praesentium inventore,
-          corporis laborum debitis quidem!
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
+          earum accusantium laudantium minus recusandae corporis in itaque, iure
+          distinctio rem cum debitis tempore dolores reiciendis voluptas
+          inventore laborum accusamus ipsam.
         </p>
       </div>
     </article>
   );
 };
 
-// Exporting
+// Exporting modules
 ReactDom.render(<Wrapper />, document.getElementById("root"));
