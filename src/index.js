3ae57d0d8da72f3ad91b8ecdbb144397a8d9822f
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-24 21:31:15 +0200

    Making an other repetition on how to center a div into a div

--- src/index.js
+++ src/index.js
@@ -7,31 +7,10 @@ import "./index.css";
 const Wrapper = () => {
   return (
     <article className="wrapper">
-      <UserItem
-        name="Optimus Tec"
-        msg="Slt bro est ce que tout va comme tu veux"
-        heure="12:30"
-      />
-      <UserItem
-        name="John Yala"
-        msg="J'etais la, mais je t'ai pas vu"
-        heure="08:45"
-      />
-    </article>
-  );
-};
-
-// Building user component
-const UserItem = (props) => {
-  return (
-    <section className="user-item">
-      <div className="avatar"></div>
-      <div className="content">
-        <p className="name">{props.name}</p>
-        <p className="msg">{props.msg}</p>
+      <div className="container">
+        <div className="avatar"></div>
       </div>
-      <p className="heure">{props.heure}</p>
-    </section>
+    </article>
   );
 };
 
