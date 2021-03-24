71dd29a5ad61b36e1ed98c9a32736d0497579b2a
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-24 20:50:48 +0200

    Handling many user-item components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
-// Importing dependdancies and modules
-import React from 'react';
-import ReactDom from 'react-dom';
+// Importing modules and dependancies
+import React from "react";
+import ReactDom from "react-dom";
 import "./index.css";
 
 // Building components
-const Wrapper = ()=>{
+const Wrapper = () => {
   return (
     <article className="wrapper">
-      <div className="user-item">
-        <div className="avatar"></div>
-        <div className="content">
-          <p className="name">Optimus Ged</p>
-          <p className="msg">Slt !!! sava ?</p>
-        </div>
-        <p className="heure">12:30</p>
-      </div>
+      <UserItem
+        name="Optimus Tec"
+        msg="Slt bro est ce que tout va comme tu veux"
+        heure="12:30"
+      />
+      <UserItem
+        name="John Yala"
+        msg="J'etais la, mais je t'ai pas vu"
+        heure="08:45"
+      />
     </article>
   );
 };
 
+// Building user component
+const UserItem = (props) => {
+  return (
+    <section className="user-item">
+      <div className="avatar"></div>
+      <div className="content">
+        <p className="name">{props.name}</p>
+        <p className="msg">{props.msg}</p>
+      </div>
+      <p className="heure">{props.heure}</p>
+    </section>
+  );
+};
+
 // Exporting modules
-ReactDom.render(<Wrapper/>, document.getElementById("root"));
+ReactDom.render(<Wrapper />, document.getElementById("root"));
