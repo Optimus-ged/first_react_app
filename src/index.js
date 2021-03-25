89404d428c1ed26d963934c1272951a5bbb03ab6
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-25 22:13:28 +0200

    Repeat on reuse of components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 // Importing dependancies and modules
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 import "./index.css";
 
 // Building components
-const Wrapper=()=>{
+const Wrapper = () => {
   return (
     <article className="wrapper">
-      <div className="user-component">
-        <div className="avatar"></div>
-        <div className="content">
-          <p className="name">Optimus ged</p>
-          <p className="msg">Eh man t'es ou ?</p>
-        </div>
-        <p className="heure">12:30</p>
+      <div className="container">
+        <UserItem name="Optimus ged" msg="Eh man t'es ou ? je te vois pas" />
+        <UserItem name="Salem Nzolani" msg="On se voit en cours !" />
+        <UserItem name="John Kuhnada" msg="Hier j'ai vu Ardy" />
+        <UserItem name="Adele Bash" msg="Jeune homme apporte moi ma tune" />
+        <UserItem name="Sisi Bash" msg="Demain je serai a l'isig" />
       </div>
     </article>
   );
 };
 
+// Building user Component
+const UserItem = (props) => {
+  return (
+    <div className="user-component">
+      <div className="avatar"></div>
+      <div className="content">
+        <p className="name">{props.name}</p>
+        <p className="msg">{props.msg}</p>
+      </div>
+      <p className="heure">12:30</p>
+    </div>
+  );
+};
+
 // Exporting modules
-ReactDom.render(<Wrapper/>, document.getElementById("root"));
+ReactDom.render(<Wrapper />, document.getElementById("root"));
