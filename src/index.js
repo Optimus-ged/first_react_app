6c30355f112c0b3c79d5fcd5d0a4ffd439b5bb3a
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-26 13:15:34 +0300

    An other repetition on item disposition with css

--- src/index.js
+++ src/index.js
@@ -1,36 +1,55 @@
 // Importing dependancies and modules
-import React from "react";
-import ReactDom from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
 
 // Building components
-const Wrapper = () => {
+const Wrapper = ()=>{
   return (
     <article className="wrapper">
       <div className="container">
-        <UserItem name="Optimus ged" msg="Eh man t'es ou ? je te vois pas" />
-        <UserItem name="Salem Nzolani" msg="On se voit en cours !" />
-        <UserItem name="John Kuhnada" msg="Hier j'ai vu Ardy" />
-        <UserItem name="Adele Bash" msg="Jeune homme apporte moi ma tune" />
-        <UserItem name="Sisi Bash" msg="Demain je serai a l'isig" />
+        <UserItem
+          name="Optimus ged"
+          msg="je suis deja a Goma, t'es ou toi ?"
+          heure="09:30"
+        />
+        <UserItem
+          name="John Yala"
+          msg="Slt man, est ce que tout va comme tu veux ?"
+          heure="12:30"
+        />
+        <UserItem
+          name="Salem Nzolani"
+          msg="Aujourd'hui les cours commence quand ?"
+          heure="08:15"
+        />
+        <UserItem
+          name="Adele Bash"
+          msg="Slt man, est ce que tout va comme tu veux ?"
+          heure="12:30"
+        />
+        <UserItem
+          name="Optimus ged"
+          msg="Slt man, est ce que tout va comme tu veux ?"
+          heure="12:30"
+        />
       </div>
     </article>
   );
 };
 
-// Building user Component
-const UserItem = (props) => {
+const UserItem=(props)=>{
   return (
-    <div className="user-component">
+    <div className="user-item">
       <div className="avatar"></div>
       <div className="content">
         <p className="name">{props.name}</p>
         <p className="msg">{props.msg}</p>
       </div>
-      <p className="heure">12:30</p>
+      <p className="heure">{props.heure}</p>
     </div>
   );
 };
 
-// Exporting modules
-ReactDom.render(<Wrapper />, document.getElementById("root"));
+// EXporting modules
+ReactDom.render(<Wrapper/>, document.getElementById("root"));
