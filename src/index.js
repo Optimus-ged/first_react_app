2f54b7fa4cd095c97fc32d1eae194676bf3db62e
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-03-26 21:52:07 +0300

    Adding user component and message component

--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
-// Importing dependancies and modules
-import React from 'react';
-import ReactDom from 'react-dom';
-import './index.css';
+// Importing depedancies and modules
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
 // Building components
-const Wrapper = ()=>{
+const Wrapper = () => {
   return (
     <article className="wrapper">
       <div className="container">
-        <UserItem
-          name="Optimus ged"
-          msg="je suis deja a Goma, t'es ou toi ?"
-          heure="09:30"
-        />
-        <UserItem
-          name="John Yala"
-          msg="Slt man, est ce que tout va comme tu veux ?"
-          heure="12:30"
-        />
-        <UserItem
-          name="Salem Nzolani"
-          msg="Aujourd'hui les cours commence quand ?"
-          heure="08:15"
-        />
-        <UserItem
-          name="Adele Bash"
-          msg="Slt man, est ce que tout va comme tu veux ?"
-          heure="12:30"
-        />
-        <UserItem
-          name="Optimus ged"
-          msg="Slt man, est ce que tout va comme tu veux ?"
-          heure="12:30"
-        />
+
+      </div>
+      <div className="container">
+        <GestMessage />
+        <div className="space-between"></div>
+        <MyMessage />
+        <div className="space-between"></div>
+        <GestMessage />
+        <div className="space-between"></div>
+        <MyMessage />
+        <div className="space-between"></div>
       </div>
     </article>
   );
 };
 
-const UserItem=(props)=>{
+// Building User item
+const UserItem =()=>{
   return (
     <div className="user-item">
       <div className="avatar"></div>
-      <div className="content">
-        <p className="name">{props.name}</p>
-        <p className="msg">{props.msg}</p>
-      </div>
-      <p className="heure">{props.heure}</p>
     </div>
   );
 };
 
-// EXporting modules
-ReactDom.render(<Wrapper/>, document.getElementById("root"));
+// Building gest Message
+const GestMessage = () => {
+  return (
+    <div className="msg-in">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    </div>
+  );
+};
+
+// Building my message
+const MyMessage = () => {
+  return (
+    <div className="msg-out">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    </div>
+  );
+};
+
+// Exporting modules
+ReactDom.render(<Wrapper />, document.getElementById("root"));
