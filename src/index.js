ef7353ea63e10b79f1c61fb5c2b9588c6fe7464d
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-02 22:42:15 +0200

    Repetition on use of sticky position

--- src/index.js
+++ src/index.js
@@ -40,11 +40,83 @@ const MyMessage = () => {
   );
 };
 
+// Building text
+const CustomText = () => {
+  return (
+    <p className="test-2">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
+      aspernatur officiis, ipsum quae similique, veniam magnam voluptatem,
+      doloribus atque fuga autem facilis nulla minima ad. Aperiam inventore illo
+      aliquid? Facilis! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Accusamus earum iste temporibus dignissimos aliquam fugiat! Dolorem ullam,
+      ea nihil fugiat, suscipit hic laborum voluptatibus odio quibusdam ab
+      necessitatibus praesentium minima. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Dolor natus velit soluta nulla aliquid tenetur cum ad
+      autem porro quam facere vel mollitia nihil, ut ea itaque, saepe minima
+      modi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero
+      adipisci accusamus, facilis asperiores pariatur in inventore voluptatem
+      dignissimos rerum molestias possimus. Maxime eum repudiandae ratione. Hic
+      harum vero ipsum iusto. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Enim, itaque debitis architecto, tenetur dicta molestiae accusantium
+      amet cumque fuga exercitationem magni quas neque! Porro non temporibus
+      sequi, laboriosam provident cupiditate? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Aut eum eos adipisci illo quaerat inventore
+      saepe cupiditate officiis libero iure earum accusantium, unde ratione nemo
+      velit in sequi rem neque? Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. At harum necessitatibus exercitationem natus enim
+      asperiores vitae, iure reprehenderit perspiciatis cum vel, consequatur
+      aliquam minus est nostrum iusto inventore neque nisi? Lorem ipsum, dolor
+      sit amet consectetur adipisicing elit. Voluptatibus, unde officiis aperiam
+      dolore fugit delectus excepturi id quasi minima molestiae! Aperiam
+      accusantium voluptates est velit, modi veniam amet suscipit repellat?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsa
+      aspernatur architecto consequuntur quaerat laborum odit repellendus, nemo
+      ex eius. Totam aperiam labore delectus incidunt nostrum animi officia
+      facere nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Fuga repudiandae omnis debitis dolores, vel modi repellat quo pariatur
+      quos, quasi praesentium repellendus eos! Natus cum officiis aspernatur
+      modi laudantium aliquid! Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Commodi aspernatur officiis, ipsum quae similique,
+      veniam magnam voluptatem, doloribus atque fuga autem facilis nulla minima
+      ad. Aperiam inventore illo aliquid? Facilis! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Accusamus earum iste temporibus dignissimos
+      aliquam fugiat! Dolorem ullam, ea nihil fugiat, suscipit hic laborum
+      voluptatibus odio quibusdam ab necessitatibus praesentium minima. Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Dolor natus velit
+      soluta nulla aliquid tenetur cum ad autem porro quam facere vel mollitia
+      nihil, ut ea itaque, saepe minima modi. Lorem ipsum, dolor sit amet
+      consectetur adipisicing elit. Vero adipisci accusamus, facilis asperiores
+      pariatur in inventore voluptatem dignissimos rerum molestias possimus.
+      Maxime eum repudiandae ratione. Hic harum vero ipsum iusto. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Enim, itaque debitis
+      architecto, tenetur dicta molestiae accusantium amet cumque fuga
+      exercitationem magni quas neque! Porro non temporibus sequi, laboriosam
+      provident cupiditate? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Aut eum eos adipisci illo quaerat inventore saepe cupiditate
+      officiis libero iure earum accusantium, unde ratione nemo velit in sequi
+      rem neque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. At
+      harum necessitatibus exercitationem natus enim asperiores vitae, iure
+      reprehenderit perspiciatis cum vel, consequatur aliquam minus est nostrum
+      iusto inventore neque nisi? Lorem ipsum, dolor sit amet consectetur
+      adipisicing elit. Voluptatibus, unde officiis aperiam dolore fugit
+      delectus excepturi id quasi minima molestiae! Aperiam accusantium
+      voluptates est velit, modi veniam amet suscipit repellat? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Nostrum ipsa aspernatur
+      architecto consequuntur quaerat laborum odit repellendus, nemo ex eius.
+      Totam aperiam labore delectus incidunt nostrum animi officia facere nisi.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga repudiandae
+      omnis debitis dolores, vel modi repellat quo pariatur quos, quasi
+      praesentium repellendus eos! Natus cum officiis aspernatur modi laudantium
+      aliquid!
+    </p>
+  );
+};
+
 // Building components
 const Wrapper = () => {
   return (
     <article className="wrapper">
-      <div className="container">
+      <div className="container-n">
         <UserItem
           name="Optimus ged"
           message="Slt big bro ! hier t'es pas venu why ?"
@@ -70,77 +142,8 @@ const Wrapper = () => {
           message="Demain je serai en ville et toi ?"
           heure="11:32"
         />
-        <h2 className="test">Optimus Tec</h2>
-        <br />
-        <p className="test-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
-          aspernatur officiis, ipsum quae similique, veniam magnam voluptatem,
-          doloribus atque fuga autem facilis nulla minima ad. Aperiam inventore
-          illo aliquid? Facilis! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Accusamus earum iste temporibus dignissimos aliquam
-          fugiat! Dolorem ullam, ea nihil fugiat, suscipit hic laborum
-          voluptatibus odio quibusdam ab necessitatibus praesentium minima.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor natus
-          velit soluta nulla aliquid tenetur cum ad autem porro quam facere vel
-          mollitia nihil, ut ea itaque, saepe minima modi. Lorem ipsum, dolor
-          sit amet consectetur adipisicing elit. Vero adipisci accusamus,
-          facilis asperiores pariatur in inventore voluptatem dignissimos rerum
-          molestias possimus. Maxime eum repudiandae ratione. Hic harum vero
-          ipsum iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Enim, itaque debitis architecto, tenetur dicta molestiae accusantium
-          amet cumque fuga exercitationem magni quas neque! Porro non temporibus
-          sequi, laboriosam provident cupiditate? Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Aut eum eos adipisci illo quaerat
-          inventore saepe cupiditate officiis libero iure earum accusantium,
-          unde ratione nemo velit in sequi rem neque? Lorem ipsum dolor, sit
-          amet consectetur adipisicing elit. At harum necessitatibus
-          exercitationem natus enim asperiores vitae, iure reprehenderit
-          perspiciatis cum vel, consequatur aliquam minus est nostrum iusto
-          inventore neque nisi? Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Voluptatibus, unde officiis aperiam dolore fugit
-          delectus excepturi id quasi minima molestiae! Aperiam accusantium
-          voluptates est velit, modi veniam amet suscipit repellat? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Nostrum ipsa aspernatur
-          architecto consequuntur quaerat laborum odit repellendus, nemo ex
-          eius. Totam aperiam labore delectus incidunt nostrum animi officia
-          facere nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Fuga repudiandae omnis debitis dolores, vel modi repellat quo pariatur
-          quos, quasi praesentium repellendus eos! Natus cum officiis aspernatur
-          modi laudantium aliquid! Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Commodi aspernatur officiis, ipsum quae similique,
-          veniam magnam voluptatem, doloribus atque fuga autem facilis nulla
-          minima ad. Aperiam inventore illo aliquid? Facilis! Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Accusamus earum iste temporibus
-          dignissimos aliquam fugiat! Dolorem ullam, ea nihil fugiat, suscipit
-          hic laborum voluptatibus odio quibusdam ab necessitatibus praesentium
-          minima. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-          natus velit soluta nulla aliquid tenetur cum ad autem porro quam
-          facere vel mollitia nihil, ut ea itaque, saepe minima modi. Lorem
-          ipsum, dolor sit amet consectetur adipisicing elit. Vero adipisci
-          accusamus, facilis asperiores pariatur in inventore voluptatem
-          dignissimos rerum molestias possimus. Maxime eum repudiandae ratione.
-          Hic harum vero ipsum iusto. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Enim, itaque debitis architecto, tenetur dicta
-          molestiae accusantium amet cumque fuga exercitationem magni quas
-          neque! Porro non temporibus sequi, laboriosam provident cupiditate?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eum eos
-          adipisci illo quaerat inventore saepe cupiditate officiis libero iure
-          earum accusantium, unde ratione nemo velit in sequi rem neque? Lorem
-          ipsum dolor, sit amet consectetur adipisicing elit. At harum
-          necessitatibus exercitationem natus enim asperiores vitae, iure
-          reprehenderit perspiciatis cum vel, consequatur aliquam minus est
-          nostrum iusto inventore neque nisi? Lorem ipsum, dolor sit amet
-          consectetur adipisicing elit. Voluptatibus, unde officiis aperiam
-          dolore fugit delectus excepturi id quasi minima molestiae! Aperiam
-          accusantium voluptates est velit, modi veniam amet suscipit repellat?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsa
-          aspernatur architecto consequuntur quaerat laborum odit repellendus,
-          nemo ex eius. Totam aperiam labore delectus incidunt nostrum animi
-          officia facere nisi. Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Fuga repudiandae omnis debitis dolores, vel modi
-          repellat quo pariatur quos, quasi praesentium repellendus eos! Natus
-          cum officiis aspernatur modi laudantium aliquid!
-        </p>
+        <h1 className="test">Optimus Tec</h1>
+        <CustomText />
       </div>
       <div className="container">
         <GestMessage />
