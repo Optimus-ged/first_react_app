2a510825d0e4d6dd739449de591d20276215998d
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-07 16:14:04 +0200

    Button component created

---
+++ src/components/button.style.js
@@ -0,0 +1,10 @@
+// Importing depedancies
+import styled from "styled-components";
+
+// Building button component
+export const Button = styled.button`
+  height: 100px;
+  width: 400px;
+  background-color: ${(props) => props.bgc};
+`;
+
