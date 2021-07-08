d86d00ec1eaadaea67616df15b4e935d5641cf90
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-08 13:44:42 +0200

    Hover event added and media screen property added

--- src/global_styles.js
+++ src/global_styles.js
@@ -30,7 +30,7 @@ export const Container = styled.div`
 // Building a button wich we donna reuse many times
 export const Button = styled.button`
   border-radius: 4px;
-  background: ${({ primary }) => (primary ? "" : "")};
+  background: ${({ primary }) => (primary ? "#4B59F7" : "#0467FB")};
   white-space: nowrap;
   padding: ${({ big }) => (big ? "12px 64px" : "10px 20px")};
   color: #fff;
@@ -38,6 +38,16 @@ export const Button = styled.button`
   outline: none;
   border: none;
   cursor: pointer;
+
+  &:hover {
+    transition: all 0.3s ease-out;
+    background: #fff;
+    background: ${({ primary }) => (primary ? "#0467FB" : "#4B59F7")};
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+  }
 `;
 
 export default GlobalStyle;
