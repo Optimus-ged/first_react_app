ee0535bc557be50c3ccd1b9adce71bc98f38ba4f
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-09 14:37:04 +0200

    Text Wrapper element created

--- src/components/info_section/info_section.elements.js
+++ src/components/info_section/info_section.elements.js
@@ -30,3 +30,12 @@ export const InfoColumn = styled.div`
   }
 `;
 
+export const TextWrapper = styled.div`
+  max-width: 540px;
+  padding-top: 0;
+  padding-bottom: 60px;
+
+  @media screen and (max-width: 768px) {
+    padding-bottom: 60px;
+  }
+`;
