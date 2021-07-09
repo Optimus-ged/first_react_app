75dc385f746f0499502ab8faa4e12997d5fa2742
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-09 20:44:38 +0200

    Heading element created

--- src/components/info_section/info_section.elements.js
+++ src/components/info_section/info_section.elements.js
@@ -48,4 +48,11 @@ export const TopLine = styled.div`
   margin-bottom: 16px;
 `;
 
+export const Heading = styled.h1`
+  margin-bottom: 24px;
+  font-size: 48px;
+  line-height: 1.1;
+  color: ${({ lightText }) => (lightText ? "#f7f8fa" : "#1c2237")};
+`;
+
 
