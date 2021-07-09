700326ccdf066d18dcef904052ba4723aeedcce8
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-09 21:05:43 +0200

    Content for home page created and added successfully

--- src/components/info_section/info_section.js
+++ src/components/info_section/info_section.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container, Button } from "../../global_styles";
 import {
   InfoSec,
   InfoRow,
   InfoColumn,
   TextWrapper,
+  TopLine,
+  Heading,
+  SubTitle,
 } from "./info_section.elements";
 
-const InfoSection = ({ lightBg, imgStart }) => {
+const InfoSection = ({
+  primary,
+  lightBg,
+  imgStart,
+  lightTopLine,
+  lightText,
+  lightTextDesc,
+  buttonLabel,
+  headLine,
+  topLine,
+  description,
+}) => {
   return (
     <>
       <InfoSec lightBg={lightBg}>
@@ -18,8 +33,10 @@ const InfoSection = ({ lightBg, imgStart }) => {
                 <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
                 <Heading lightText={lightText}>{headLine}</Heading>
                 <SubTitle lightTextDesc={lightTextDesc}>{description}</SubTitle>
-                <Link to="/sign-up" >
-                  <Button big fontBig primary={primary} >{buttonLabel}</Button>
+                <Link to="/sign-up">
+                  <Button big fontBig primary={primary}>
+                    {buttonLabel}
+                  </Button>
                 </Link>
               </TextWrapper>
             </InfoColumn>
--- src/pages/home_page/data.js
+++ src/pages/home_page/data.js
@@ -1,3 +1,13 @@
 export const homeObjOne = {
-  lightBg : false
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightText:true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We Help business owners increase their revenue. Our Team of unique specialist can help you achieve your business goals",
+  headLine : "Lead Generation specialists for online business",
+  topLine : "Market Agency",
 };
