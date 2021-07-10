5362cc37d215c0c797dcd22f3819bcb2985ff44a
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-10 09:12:58 +0200

    Three other objects created for the home page

--- src/pages/home_page/data.js
+++ src/pages/home_page/data.js
@@ -1,4 +1,10 @@
-import test from "../../images/toph-1.svg";
+// Importing images
+import img1 from "../../images/toph-1.svg";
+import img2 from "../../images/toph-2.svg";
+import img3 from "../../images/toph-3.svg";
+import img4 from "../../images/toph-4.svg";
+
+// Building objects
 export const homeObjOne = {
   primary: true,
   lightBg: false,
@@ -11,7 +17,59 @@ export const homeObjOne = {
     "We Help business owners increase their revenue. Our Team of unique specialist can help you achieve your business goals",
   headLine: "Lead Generation specialists for online business",
   topLine: "Market Agency",
-  img: test,
+  img: img1,
   start: "",
   alt: "Image",
 };
+
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We Help business owners increase their revenue. Our Team of unique specialist can help you achieve your business goals",
+  headLine: "Lead Generation specialists for online business",
+  topLine: "Market Agency",
+  img: img2,
+  start: "",
+  alt: "Image",
+};
+
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We Help business owners increase their revenue. Our Team of unique specialist can help you achieve your business goals",
+  headLine: "Lead Generation specialists for online business",
+  topLine: "Market Agency",
+  img: img3,
+  start: "",
+  alt: "Image",
+};
+
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We Help business owners increase their revenue. Our Team of unique specialist can help you achieve your business goals",
+  headLine: "Lead Generation specialists for online business",
+  topLine: "Market Agency",
+  img: img4,
+  start: "",
+  alt: "Image",
+};
+
