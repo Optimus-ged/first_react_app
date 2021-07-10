b1b1ff5f906031ab2ba9166264d0443e2d2e8695
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-10 11:22:13 +0200

    Footer links container element created

--- src/components/footer/footer.elements.js
+++ src/components/footer/footer.elements.js
@@ -33,20 +33,42 @@ export const FooterSubText = styled.p`
 `;
 
 export const Form = styled.form`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    @media sreen and (max-width:820px){
-        flex-direction: column;
-        width: 80%;
-    }
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+    width: 80%;
+  }
 `;
 
 export const FormInput = styled.input`
-    padding: 10px 20px;
-    border-radius: 2px;
-    margin-right:10px ;
-    font-size: 16px;
-    border: 1px solid #FFF;
+  padding: 10.5px 20px;
+  border-radius: 2px;
+  margin-right: 10px;
+  font-size: 16px;
+  border: 1px solid #fff;
+
+  &::placeholder {
+    color: #242424;
+  }
+
+  @media screen and (max-width: 820px) {
+    width: 100%;
+    margin: 0 0 16px 0;
+  }
 `;
+
+export const FooterLinksContainer = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    display: flex;
+    justify-content: center;
+
+    @media screen and (max-width:820px){
+        padding-top: 32px;
+    }
+`;
+
+
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,6 +5,8 @@ import {
   FooterSubscription,
   FooterSubHeading,
   FooterSubText,
+  Form,
+  FormInput,
 } from "./footer.elements";
 
 const Footer = () => {
@@ -18,10 +20,45 @@ const Footer = () => {
           <FooterSubText>You can unsubscribe at any time.</FooterSubText>
         </FooterSubscription>
         <Form>
-          <FormInput name="email" type="email" placeHolder="Your email">
-            <Button fontBig>Subscribe</Button>
-          </FormInput>
+          <FormInput name="email" type="email" placeholder="Your email" />
+          <Button fontBig>Subscribe</Button>
         </Form>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact us</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social media</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of service</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
       </FooterContainer>
     </>
   );
