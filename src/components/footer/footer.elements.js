7b79c28329656dfb54d4590a9c8b9e449322d088
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-10 12:12:52 +0200

    Social media logo component created

--- src/components/footer/footer.elements.js
+++ src/components/footer/footer.elements.js
@@ -129,4 +129,13 @@ export const SocialMediaWrap = styled.div`
   }
 `;
 
-
+export const SocialLogo = styled(Link)`
+  color: #fff;
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+`;
