d685b2a2c757d8928d045a9282ea63b749d320a6
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-18 10:53:31 +0200

    Hellowordl component created and used

--- src/react_trainings/App.js
+++ src/react_trainings/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import { HelloWorld } from "./components/Hello";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <HelloWorld />
         <Greet name="Adele">
           <p>props children</p>
         </Greet>
         <Greet name="Salem" />
         <Greet name="John" />
-        <Welcome  name="Adele"/>
+        <Welcome name="Adele" />
       </div>
     );
   }
