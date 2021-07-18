2bbd6be6c6b084791546706e0220989df3f1e726
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-18 12:51:29 +0200

    Handling of setState

--- src/react_trainings/App.js
+++ src/react_trainings/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import { HelloWorld } from "./components/Hello";
-import Message from "./components/Message";
+// import Greet from "./components/Greet";
+// import Welcome from "./components/Welcome";
+// import { HelloWorld } from "./components/Hello";
+// import Message from "./components/Message";
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <>
         {/* <HelloWorld />
         <Greet name="Adele">
           <p>props children</p>
@@ -15,8 +16,9 @@ class App extends Component {
         <Greet name="Salem" />
         <Greet name="John" />
         <Welcome name="Adele" /> */}
-        <Message/>
-      </div>
+        {/* <Message/> */}
+        <Counter/>
+      </>
     );
   }
 }
---
+++ src/react_trainings/components/Counter.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+//   incrementCounter() {
+//     this.setState(
+//       {
+//         count: this.state.count + 1,
+//       },
+//       () => console.log(this.state.count)
+//     );
+//   }
+
+  incrementCounter() {
+    this.setState((prevState, props) => ({
+      count: this.state.count + 1,
+    }));
+  }
+
+  incrementFive() {
+    this.incrementCounter();
+    this.incrementCounter();
+    this.incrementCounter();
+    this.incrementCounter();
+    this.incrementCounter();
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.count}</h1>
+        <button onClick={() => this.incrementFive()}>Incrementer</button>
+      </>
+    );
+  }
+}
+
+export default Counter;
