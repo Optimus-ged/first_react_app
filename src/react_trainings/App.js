71f42ad5a4a69ff8d30c2940f82cc00625403593
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-18 09:28:49 +0200

    App component created successfully for react trainings exercise

---
+++ src/react_trainings/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import App from "./components/Greet";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <App />
+      </>
+    );
+  }
+}
+
+export default App;
