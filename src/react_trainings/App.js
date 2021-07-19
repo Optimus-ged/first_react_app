bcd7a1e7673debc9b55d66e57e30830b70914b51
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-20 00:18:13 +0200

    Fragment component Handled successfully

--- src/react_trainings/App.js
+++ src/react_trainings/App.js
@@ -1,45 +1,11 @@
 import React, { Component } from "react";
-// import NameList from "./components/PersonList/NameList";
-// import Greet from "./components/Greet";
-// import Welcome from "./components/Welcome";
-// import { HelloWorld } from "./components/Hello";
-// import Message from "./components/Message";
-// import Counter from './components/Counter';
-// import FunctionClick from './components/FunctionClick';
-// import ClassClick from "./components/ClassClick";
-// import EventBind from "./components/EventBind";
-// import ParentComponent from "./components/ParentAndChild/ParentComponent";
-// import UserGreeting from "./components/UserGreeting";
-// import StyleSheet from "./components/CssBasics/StyleSheet";
-// import InLine from "./components/CssBasics/InLine";
-// import "./components/CssBasics/appStyles.css";
-// import style from "./components/CssBasics/appStyles.module.css";
-// import Form from "./components/HandlingForm";
-import LifecycleA from "./components/LifecycleLearning/LifecycleA";
+import FragmentDemo from "./components/FragmentLearning/FragmentDemo";
 
 class App extends Component {
   render() {
     return (
       <>
-        {/* <HelloWorld /> */}
-        {/* <Greet name="Adele">
-          <p>props children</p>
-        </Greet> */}
-        {/* <Greet name="Salem" /> */}
-        {/* <Greet name="John" /> */}
-        {/* <Welcome name="Adele" /> */}
-        {/* <Message/> */}
-        {/* <Counter/> */}
-        {/* <FunctionClick/> */}
-        {/* <ClassClick/> */}
-        {/* <EventBind/> */}
-        {/* <ParentComponent/> */}
-        {/* <UserGreeting/> */}
-        {/* <NameList /> */}
-        {/* <StyleSheet className={true}/>
-        <InLine/>
-        <h1 className={style.success}>Success</h1> */}
-        <LifecycleA />
+        <FragmentDemo />
       </>
     );
   }
---
+++ src/react_trainings/components/FragmentLearning/FragmentDemo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Table from "./Table";
+
+class FragmentDemo extends Component {
+  render() {
+    return <Table />;
+  }
+}
+
+export default FragmentDemo;
