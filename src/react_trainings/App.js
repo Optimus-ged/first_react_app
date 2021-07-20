ae8953d6e2c963f62875c3c5ae4bba50fdf09caf
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-07-20 13:53:06 +0200

    Making use of on mouse hover handler for the mouse event

--- src/react_trainings/App.js
+++ src/react_trainings/App.js
@@ -16,7 +16,7 @@ class App extends Component {
           <Hero heroName="Jocker" />
         </ErrorBoundary> */}
         <ClickCounter />
-        
+        <HoverCounter/>
       </>
     );
   }
